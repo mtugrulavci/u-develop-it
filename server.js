8aef17539ad8d91989d58876abadd5c98085785d
mtugrulavci <mustafa.tugrulavci@alcumus.com>
2021-07-02 18:51:19 -0400

    database connection is completed

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mysql = require('mysql2');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app =express();
@@ -6,12 +7,26 @@ const app =express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-
-
 // Default response for any other request (Not Found)
 app.use((req,res)=>{
     res.status(404).end();
 })
+
+//connect to databae
+const db = mysql.createConnection(
+    {
+     host: "localhost",
+     //your MySQL username,
+     user: "root",
+     //Your MySQL password, I didn't use password since I have no password!
+    // password : ""; 
+     database: "election"   
+    },
+    console.log("Connected to the election database.")
+
+);
+
+
 
 app.listen(PORT, ()=>{
     console.log(`Server running on port ${PORT}`);
